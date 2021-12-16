e9dddcde819481b5647c13ba4fa372a672e7ce51
Artem Arshava <art.mem.tra@gmail.com>
2021-12-16 18:38:14 +0200

    added indexator

--- SeaBattle/Field.cs
+++ SeaBattle/Field.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeaBattle.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,23 @@ namespace SeaBattle
 {
     public class Field
     {
-
+        private Help helper;
+        private List<Ship> ships;
+        public List<Point> points;
+        private int Height { get; set; }
+        private int Width { get; set; }
+        public Field(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+        public Ship this[Point point]
+        {
+            get
+            {
+                var notEmpty = points.Find((p) => p.X == point.X && p.Y == point.Y && p.quadrant == point.quadrant);
+                return notEmpty.ship;
+            }
+        }
     }
 }
