ec3c55f38aedfded2c9b12970eff0c8bedfaef13
Artem Arshava <art.mem.tra@gmail.com>
2021-12-16 18:55:59 +0200

    created func AddShip

--- SeaBattle/Field.cs
+++ SeaBattle/Field.cs
@@ -26,6 +26,37 @@ namespace SeaBattle
                 var notEmpty = points.Find((p) => p.X == point.X && p.Y == point.Y && p.quadrant == point.quadrant);
                 return notEmpty.ship;
             }
+            set 
+            {
+                if (point._isEmpty)
+                {
+                    
+                }
+            }
         }
+        public void AddShip(Ship ship, Point point)
+        {
+            if (points.Count==0)
+            {
+                ships.Add(ship);
+                points.Add(point);
+            }
+            else
+            {
+                foreach (var item in points)
+                {
+                    if (item.X != point.X && item.Y != point.Y || point.X > Width || point.Y > Height)
+                    {
+                        ships.Add(ship);
+                        points.Add(point);
+                    }
+                    else
+                    {
+                        Console.WriteLine("This point is already taken ");
+                    }
+                }             
+            }
+        }
+
     }
 }
