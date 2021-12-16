c7bb5fc558c13cbaaedc86c1816565c4cbf100e7
Artem Arshava <art.mem.tra@gmail.com>
2021-12-16 16:32:35 +0200

    first init

---
+++ SeaBattle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeaBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
